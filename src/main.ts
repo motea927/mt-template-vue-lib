04f03500947076be1f847927d4a5a4c9dcfaee43
motea927 <motea927@yahoo.com.tw>
2023-01-26 23:51:42 +0800

    feat(init): add playground for dev

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
+import type { App as AppType } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+const isDev = import.meta.env.MODE === 'development'
+if (isDev) {
+  const { createApp } = await import('vue')
+  const PlayGroundApp = (await import('../playground/App.vue')).default
+  await import('../playground/assets/main.css')
+  createApp(PlayGroundApp).mount('#app')
+}
 
-createApp(App).mount('#app')
+export default {
+  install(app: AppType) {
+    app.component('MtFocus', App)
+  },
+}
