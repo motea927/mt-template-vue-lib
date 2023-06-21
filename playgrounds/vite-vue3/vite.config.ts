1faeb18d991c93d33656380ee3aedfbe385d9573
motea927 <motea927@yahoo.com.tw>
2023-06-22 00:03:01 +0800

    fix: fix local module not update

--- playgrounds/vite-vue3/vite.config.ts
+++ playgrounds/vite-vue3/vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig(() => ({
-  plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx()],
+  server: {
+    watch: {
+      usePolling: true, // For Docker.
+      ignored: ['!**/node_modules/mt-template-vue-lib-core/**']
+    }
+  },
+  optimizeDeps: {
+    exclude: ['mt-template-vue-lib-core']
+  }
 }))
